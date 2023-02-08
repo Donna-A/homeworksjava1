9490cfc1eb6110b09467b0a409c5b94f8189783a
Donna <annamirnow@gmail.com>
2023-02-08 15:57:57 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("int eaters = 5; // сколько людей будут есть\n" +
+                "\n" +
+                "        int water = 3000; // миллилитров воды\n" +
+                "        int potatoes = 5; // картофелин\n" +
+                "        int chicken = 6; // куриных бёдер\n" +
+                "        int spices = 10; // ложек специй\n" +
+                "\n" +
+                "        System.out.println(\"Сварили суп. На одного человека вышло:\");\n" +
+                "        System.out.println((water / eaters) + \" миллилитров(а) воды\");\n" +
+                "        System.out.println((potatoes / eaters) + \" картофелин(а/ы)\");\n" +
+                "        System.out.println((chicken / eaters) + \" куриных(ое) бёдер(ро)\");\n" +
+                "        System.out.println((spices / eaters) + \" ложек(ка) специй\");");
+
+    }
+}
